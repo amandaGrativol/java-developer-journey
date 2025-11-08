1c4bcf00c57f205174a4bdb90931cc4ff9a6ba37
Amanda Aparecida Grativol Rocha <88804457+amandaGrativol@users.noreply.github.com>
2025-11-08 00:13:34 -0300

    Create HelloWorld.java

---
+++ basics/variables-types-operators/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Java Journey Started!");
+        System.out.println("Date: " + java.time.LocalDate.now());
+        
+        // Test operations
+        int result = 10 + 5;
+        System.out.println("10 + 5 = " + result);
+    }
+}
