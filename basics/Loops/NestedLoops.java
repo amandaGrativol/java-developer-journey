1b105f9d628b74ffd49d36e601b3d2639a16943e
Amanda Aparecida Grativol Rocha <88804457+amandaGrativol@users.noreply.github.com>
2025-11-12 11:00:44 -0300

    Update NestedLoops.java

--- basics/Loops/NestedLoops.java
+++ basics/Loops/NestedLoops.java
@@ -5,7 +5,7 @@
  */
 import java.util.Scanner;
 
-public class Main {
+public class NestedLoops {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
