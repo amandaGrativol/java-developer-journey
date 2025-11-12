12ff3510f4440b736f8f45398694bd8d13e9db81
Amanda Aparecida Grativol Rocha <88804457+amandaGrativol@users.noreply.github.com>
2025-11-12 11:00:03 -0300

    Update DoWhile.java

--- basics/Loops/DoWhile.java
+++ basics/Loops/DoWhile.java
@@ -5,7 +5,7 @@
  */
 import java.util.Scanner;
 
-public class DoWhileExplained {
+public class DoWhile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
