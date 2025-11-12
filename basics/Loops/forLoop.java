57d2b4ea9f90dc5dede4f344c6ccaf988eac2df0
Amanda Aparecida Grativol Rocha <88804457+amandaGrativol@users.noreply.github.com>
2025-11-12 11:01:15 -0300

    Update forLoop.java

--- basics/Loops/forLoop.java
+++ basics/Loops/forLoop.java
@@ -5,7 +5,7 @@
  */
 import java.util.Scanner;
 
-public class Main {
+public class forLoop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
